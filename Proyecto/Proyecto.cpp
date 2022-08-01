f76e89bbeb3e31f00d2f499c2a8d3462f374c477
Esteban <estebansalas402@gmail.com>
2022-07-31 22:31:46 -0600

    Avance proyecto 31/07/2022

--- Proyecto/Proyecto.cpp
+++ Proyecto/Proyecto.cpp
@@ -6,15 +6,11 @@
 using namespace std;
 // Ejercicio2
 int main(int argc, char** argv) {
-	setlocale(LC_CTYPE, "Spanish");
-	int a = 0;
-	bool verificado;
-	string respuesta[8];
-	////////////DECLARACION DE PREGUNTAS
-	
-
-
-	string millonario[8][2]{
+	int NumeroRandom = 0;//se declara la variable que generara los numeros aleatorios para la pregunta
+	string respuesta[11];//array para almacenar la respuesta del usuario
+	string RespuestaDefinitiva;
+	setlocale(LC_CTYPE, "Spanish");//este codigo junto con la libreria #include <clocale> para usar caractes como ççÒ
+	/*Aqui se declara las preguntas*/string millonario[11][2]{
 		
 		 "0.Quien descubrio America\n  A:Magallanes\n  B:Colon\n  C:Bush\n  D:Araya"
 		  ,"B"
@@ -37,21 +33,46 @@ int main(int argc, char** argv) {
 		,"6.En que pais esta ubicada **Capital Gate**,la torre mas inclinada del mundo\n  A:Jordania\n  B:Arabia Saudita\n  C:Marruecos\n  D:Emiratos Arabes Unidos"
 		,"D"
 		/////
-		,"7.Cal fue la moneda oficial de italia durante mas de cien aÒos antes de ser reemplazada por el euro?\n  A:Corona\n  B:Rublo\n  C:Libra\n  D:Lira"
+		,"7.øC˙al fue la moneda oficial de italia durante mas de cien aÒos antes de ser reemplazada por el euro?\n  A:Corona\n  B:Rublo\n  C:Libra\n  D:Lira"
 		,"D"
+		//////
+		,"8.øC˙antas aristas tiene un cubo?\n  A:16\n  B:12\n  C:8\n  D:4"
+		,"B"
+		//////
+		,"9.Durante el programa de la NASA \"Apolo\",øcu·ntos astronautas pisaron la Luna?\n  A:3\n  B:6\n  C:9\n  D:12"
+		,"D"
+		//////
+		,"10.øCu·l es el gas que infla las bolsas de aire,o \"airbags\",que se encuentran en los automÛviles?\n  A:Helio\n  B:NitrÛgeno\n  C:OxÌgeno\n  D:HidrÛgeno"
+		,"B"
 	};
 
 
-	bool pregunta[8]{ false,false,false,false,false,false,false,false};
+	bool verificacionRepetidos[11]{ false,false,false,false,false,false,false,false,false,false,false};//
 
-	for (int i = 0;i<8;i++) {
-	a = rand() % 8;
-	cout << a<<endl;
-		verificado=pregunta[a];
-		if (verificado==false) {
-			cout <<""<< millonario[a][0] << endl;
-			cin >> respuesta[a];
-			pregunta[a] = true;
+	for (int i = 0;i<11;i++) {//Aqui se imprimen las preguntas y se valida que no se repitan
+	NumeroRandom = rand() % 13;//Variable que genera numeros aleatorios
+	cout << NumeroRandom<<endl;
+		
+		if (verificacionRepetidos[NumeroRandom]==false) {//
+			cout << millonario[NumeroRandom][0] << endl;
+			cin >> respuesta[NumeroRandom];
+			
+			do {
+				cout << "Selecciona una respuesta" << endl;
+				cout << "Respuesta definitiva?\nDigita SI o NO tambien puedes acceder a uno de tus comodines" << endl;
+				cin >> RespuestaDefinitiva;
+				
+				if (RespuestaDefinitiva == "SI" || RespuestaDefinitiva == "si") {
+					respuesta[NumeroRandom] = respuesta[NumeroRandom];
+				}
+				else if (RespuestaDefinitiva == "Comodin") {
+					respuesta[NumeroRandom] = "comodin";
+
+				}
+
+			} while (RespuestaDefinitiva != "SI" || RespuestaDefinitiva != "SI");
+			
+			verificacionRepetidos[NumeroRandom] = true;
 		}
 		else {
 			i--;
